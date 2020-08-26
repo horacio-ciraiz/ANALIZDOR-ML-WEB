1850f64862a4e4378f2af49ed41eafd010234752
Horacio Ciraiz <horacio.ciraiz@gmail.com>
2020-08-25 23:32:33 -0600

    Analizador Lexico JS
    Analisis ID, DIGITO

--- Analizadores/LexicoJS.py
+++ Analizadores/LexicoJS.py
@@ -13,7 +13,7 @@ def ValidarSimbolo(caracter):
     elif varascii== 92 :
         return 4 # \ diagonal       
     elif varascii==39 or varascii==37 or varascii==61 or varascii==59 or varascii==62 or varascii==60 or varascii==40 or varascii==41 or varascii==125 or varascii==123 or varascii==43 or varascii==45  or varascii==46 or varascii==44 or varascii==33 or varascii==38 or varascii==124 :
-        return 5 #simbolos ' % = ; > < ( ) } { + - / . , ! & |   
+        return 5 #simbolos ' % = ; > < ( ) } { + - . , ! & |   
     elif  varascii>=0 and varascii<=32 :
         return 6 #controles de linea  
     else: 
@@ -24,15 +24,81 @@ def ValidarSimbolo(caracter):
 
 def Analizar(cadena):
     indice=0
+    token=""
+    cadenaJS=""
     while indice < len(cadena):
         letra = cadena[indice]
+        validacion = ValidarSimbolo(letra)
 
-        num = ValidarSimbolo(letra)
-        print (num)
-        indice += 1
+        #----------------------ID---------------------
+        if validacion==1 : #Letra en ID A-B
+            print ("--------------ID---------")
+            token=""
+            #print("Letra")
+            token=token+letra
+            bandera=0
+            indice += 1
+            while bandera==0: #B-B
+                if indice==len(cadena):
+                    break
+                letra = cadena[indice]
+                validacion = ValidarSimbolo(letra)
+
+                if validacion==1: # Letra
+                    #print("letra")
+                    token=token+letra
+                    indice += 1
+                elif validacion==2: #Digito
+                    token=token+letra
+                    #print("digito")
+                    indice += 1
+                else:
+                              
+                    bandera=1
+            print (token)
+            
+        #----------------------Digito-----------------
+        elif validacion==2: #Digito en A - C
+            print("----------Digito-----------")
+            token=""
+            token=token+letra
+            bandera=0
+            indice += 1
+
+            while bandera==0: #B-B
+                if indice==len(cadena):
+                    break
+                letra = cadena[indice]
+                validacion = ValidarSimbolo(letra)
+
+                if validacion==2: # Letra
+                    #print("letra")
+                    token=token+letra
+                    indice += 1
+                else:
+                            
+                    bandera=1
+            print (token)
+
+
+          
+        #----------------------Simbolo-----------------
+        elif validacion==5: #simbolos     
+            break
+        elif validacion==6: #control de lineas
+            
+            indice += 1
+        else:
+            print("Error Lexico")
+            print(letra)
+            indice += 1
+            
+        
+
+
+
     
-    
-Analizar('Horacio* % = ; > < ( ) } { + -  . , ! & | @')
+Analizar('Horacio2 @Hola#    223#2         HOla')
 
 
 
