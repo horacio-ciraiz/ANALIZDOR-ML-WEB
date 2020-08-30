80a2136088815bd490c5d312ba1dac19c3b89051
Horacio Ciraiz <horacio.ciraiz@gmail.com>
2020-08-30 13:08:17 -0600

    Analisadores Lenguaje HTML,CSS y JS
    analizadores completos

--- Analizadores/ErrorLexicoCSS.py
+++ Analizadores/ErrorLexicoCSS.py
@@ -1,1 +1,12 @@
-from ErrorLexicoJS import ErrorLexJS
+class ErrorLexCSS:
+
+    def __init__(self, simbolo, fila, columna):
+        self.simbolo = simbolo
+        self.fila = fila
+        self.columna = columna
+
+    #def __repr__(self):
+      #  return str(self.__dict__)
+    def __str__(self):
+        return "simbolo=%s fila=%s columna=%s" % (self.simbolo, self.fila, self.columna)
+
