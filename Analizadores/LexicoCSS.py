bc5d5af9cd1b29e1024a711e666d9d3ec907bcda
Horacio Ciraiz <horacio.ciraiz@gmail.com>
2020-08-31 10:01:47 -0600

    Analisadores 3 Completado
    3 analizadores completados

--- Analizadores/LexicoCSS.py
+++ Analizadores/LexicoCSS.py
@@ -1,4 +1,4 @@
-from ErrorLexicoHTML import ErrorLexHTML
+from ErrorLexicoCSS import ErrorLexCSS
 
 
 lista_error = list()
@@ -14,9 +14,9 @@ def ValidarSimbolo(caracter):
         return 2 #numeros    
     elif varascii== 42 :
         return 3 # * asterisco   
-    #elif varascii== 47 :
-        #return 4 # / diagonal   
-    elif  varascii==92 or varascii==47 or varascii==37 or varascii==61 or varascii==58 or varascii==59 or varascii==62 or varascii==60 or varascii==40 or varascii==41 or varascii==125 or varascii==123 or varascii==43 or varascii==45  or varascii==46 or varascii==44 or varascii==33 or varascii==38 or varascii==124 :
+    elif varascii== 47 :
+        return 4 # / diagonal   
+    elif  varascii==92 or varascii==35 or varascii==37 or varascii==61 or varascii==58 or varascii==59 or varascii==62 or varascii==60 or varascii==40 or varascii==41 or varascii==125 or varascii==123 or varascii==43 or varascii==45  or varascii==46 or varascii==44 or varascii==33 or varascii==38 or varascii==124 :
         return 5 #simbolos / \ ' % = ; > < ( ) } { + - . , ! & |   
     elif  varascii==32 :
         return 6 #espacio en blanco
@@ -93,7 +93,7 @@ def AnalizarCSS(cadena):
                     print("---------Error Lexico---------")
                     token=token+letra
 
-                    NuevoError= ErrorLexHTML(str(letra),str(fila),str(columna))
+                    NuevoError= ErrorLexCSS(str(letra),str(fila),str(columna))
                     lista_error.append(NuevoError)
 
                     #print("digito")
@@ -144,7 +144,7 @@ def AnalizarCSS(cadena):
                     print("---------Error Lexico---------")
                     token=token+letra
 
-                    NuevoError= ErrorLexHTML(str(letra),str(fila),str(columna))
+                    NuevoError= ErrorLexCSS(str(letra),str(fila),str(columna))
                     lista_error.append(NuevoError)
                     #print("digito")
                     indice += 1
@@ -274,7 +274,7 @@ def AnalizarCSS(cadena):
                 if indice==len(cadena):
                     print("ERROR CADENA 0")
                     
-                    NuevoError= ErrorLexHTML(str(letra),str(fila),str(columna))
+                    NuevoError= ErrorLexCSS(str(letra),str(fila),str(columna))
                     lista_error.append(NuevoError)
                     break
                 letra = cadena[indice]
@@ -290,7 +290,7 @@ def AnalizarCSS(cadena):
                 elif validacion==7:
                     
                     print("ERROR CADENA 1")
-                    NuevoError= ErrorLexHTML("Salto-Linea",str(fila),str(columna))
+                    NuevoError= ErrorLexCSS("Salto-Linea",str(fila),str(columna))
                     lista_error.append(NuevoError)
                     token=token+letra
                     indice+=1
@@ -308,11 +308,131 @@ def AnalizarCSS(cadena):
                 
             print (token)
             token=""
+        #---------------Comentario  ---------------------
+        elif validacion==4: #Diagonal / en A - G
+            
+            print("----------Comentario  ----------")
+            #token=""
+            token=token+letra
+            bandera=0
+            indice += 1
+            columna+=1
+
+            if indice==len(cadena):
+                break
+            else:
+                letra = cadena[indice]
+                validacion = ValidarSimbolo(letra)
+                #--------------------Comentario de Linea-------------------
+
+                #-------------------Comentario Multilinea--------------
+                if validacion==3: # * Asterisco    
+                    
+                    print("----------Inicio Comentario MultiLinea")
+                    while bandera==0: #G-I
+                        if indice==len(cadena):
+                            break
+
+                        letra = cadena[indice]
+                        validacion = ValidarSimbolo(letra)
+                        
+
+                        if validacion==3: #*Asterisco 
+                            
+                            token=token+letra
+                            indice += 1 
+                            columna+=1
+                            if indice==len(cadena):
+                                break
+                            
+                            letra = cadena[indice]
+                            validacion = ValidarSimbolo(letra)
+                            
+
+                            if validacion==4: # / Diagonal
+                                
+                                token=token+letra
+                                indice += 1
+                                columna+=1 
+                                #print("----------Fin Comentario Multilinea--------")                               
+                                break
+                        
+                            else:
+                                
+                                token=token+letra
+                                indice += 1 
+                                columna+=1 
+                            
+                        #Salida de Comentario
+
+                        else: 
+                                validacion=ValidarSimbolo(letra)
+                                if validacion==7:
+                                    token=token+letra
+                                    indice += 1 
+                                    columna+=1 
+                                    fila+=1
+                                    columna=1
+                                else:    
+                                    token=token+letra
+                                    indice += 1 
+                                    columna+=1    
+                        # Salida Digito
+                    print (token)
+                    token=""
+                    print("----------Fin Comentario Multilinea--------")
+
+
+                else:
+                    
+                    print("Error Lexico Comentario")    
+                    token=token+letra
+                    indice+=1
+                    columna+=1
+
+                    while bandera==0: #C-C
+                        if indice==len(cadena):
+                            break
+
+                        letra = cadena[indice]
+                        validacion = ValidarSimbolo(letra)
+
+                        if validacion==7:
+                            NuevoError= ErrorLexCSS("Salto-Linea",str(fila),str(columna))
+                            lista_error.append(NuevoError)
+                        else:
+                            NuevoError= ErrorLexCSS(str(letra),str(fila),str(columna))
+                            lista_error.append(NuevoError)
+                        
+
+                        if validacion==7: # Salto de Linea
+                            
+                            #fila+=1
+                            columna=1
+                            bandera=1
+                        #Salida de Comentario
+
+                        else: 
+                            token=token+letra
+                            indice += 1
+                            columna+=1     
+                        # Salida Digito
+                    print (token)
+
+                    token=""
+                    
+
+
+
+       
                 
             
   
         else:
-            print("Lo que sea ")   
+            NuevoError= ErrorLexCSS(str(letra),str(fila),str(columna))
+            lista_error.append(NuevoError)
+
+            print("---------Error Lexico")   
             print(letra)
             indice+=1
             columna+=1 
@@ -340,7 +460,10 @@ AnalizarCSS(
 " \n background-color: rgba(220, 53, 69, 0.9);" #12
 " \n font: 76% arial, sans-serif;" #13
 " \n background: 0 0 0 0.2rem rgba(51, 51, 51, 0.25);" #14
-" \n }") #15
+" \n }"
+" \n /* %--- Comentario de una sola línea ---$ */ "
+" \n /* %--- Comentario de \n una sola línea ---$ */ @"
+) #15
 
 ErroresLexicosCSS()
 
